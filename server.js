2451433f2bd9282b0968824efffb6865539a4cd8
Ankit Yande <ankityande@gmail.com>
2025-03-30 16:05:15 -0500

    Refactored code to be more efficient

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
-const app = require("express")
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, {
-    cors: {
-        origin: "https://ankityande.github.io/socket-paint-client/",
-        credentials: true
-    },
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://ankityande.github.io/socket-paint-client/",
+    // origin: "http://localhost:3000",
+    credentials: true,
+  },
 });
 
+const canvasState = {}; // Store canvas state per room
+
 io.on("connection", (socket) => {
-    // console.log("User Connected", socket.id);
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
 
-    socket.on('canvas-data', (image, room) => {
-        if (room) {
-            // console.log("sending to room", room, "connected to room", socket.rooms)
-            socket.to(room).emit('canvas-data', image);
-        }
-        // else {
-        //     // console.log("broadcasting")
-        //     socket.broadcast.emit('canvas-data', image)
-        // }
-    })
+  socket.on("draw-data", ({ x, y, lastX, lastY, color, size, room }) => {
+    socket.to(room).emit("draw-data", { x, y, lastX, lastY, color, size });
+  });
 
-    socket.on('clear-canvas', (room) => {
-        console.log("clear in server")
-        if (room) {
-            socket.to(room).emit('clear-canvas');
-        }
-    })
+  socket.on("clear-canvas", (room) => {
+    io.to(room).emit("clear-canvas");
+    canvasState[room] = null; // Clear stored canvas state
+  });
 
-    socket.on('join-room', (room, cb) => {
-        socket.join(room)
-        cb(`Joined ${room}`)
-    })
+  socket.on("save-canvas", ({ room, dataUrl }) => {
+    canvasState[room] = dataUrl;
+  });
 
-    socket.on('disconnect', function () {
-        // console.log(socket.id, 'disconnected');
-    });
+  socket.on("request-canvas", (room) => {
+    if (canvasState[room]) {
+      socket.emit("load-canvas", canvasState[room]);
+    }
+  });
 });
 
 const port = process.env.PORT || 3001;
